6b31d9c860f89ab41e84fe46a4bfd6f2d4901ec1
midchildan <midchildan+git@gmail.com>
2017-03-08 17:22:07 +0900

    gdb: Make it easier to switch between peda/pwndbg.

--- home/.bashrc
+++ home/.bashrc
@@ -26,6 +26,8 @@ alias egrep='egrep --color=auto'
 alias ls='ls -F --color=auto'
 alias ll='ls -lh'
 alias la='ls -lAh'
+alias peda='GDB_USE_PEDA=1 gdb'
+alias pwndbg='GDB_USE_PWNDBG=1 gdb'
 alias xmonad-replace='nohup xmonad --replace &> /dev/null &'
 
 #############
--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ main() {
   cd "$DOTFILE_DIR"
 
   echo "$(tput bold)== Cloning submodules ==$(tput sgr0)"
-  git submodule update --init --recursive
+  git submodule update --init
 
   echo "$(tput bold)== Installing configuration ==$(tput sgr0)"
   setup::shell
@@ -123,6 +123,7 @@ setup::misc() {
   install_symlink ".ipython/profile_default/ipython_config.py"
   install_symlink ".latexmkrc"
   install_symlink ".local/opt/peda"
+  install_symlink ".local/opt/pwndbg"
   install_symlink ".local/share/zsh/site-functions"
   install_symlink ".mikutter/plugin"
   install_symlink ".nixpkgs/config.nix"
