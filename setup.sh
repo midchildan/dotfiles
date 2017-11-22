4d3d2beb6877828e06aad567708f2b9a7a12651f
midchildan <midchildan+git@gmail.com>
2017-11-22 15:31:22 +0900

    tig: Initial configuration

--- setup.sh
+++ setup.sh
@@ -78,6 +78,7 @@ setup::misc() {
   install::default ".config/latexmk/latexmkrc"
   install::default ".config/ranger/rc.conf"
   install::default ".config/ranger/scope.sh"
+  install::default ".config/tig/config"
   install::default ".config/zathura/zathurarc"
   install::default ".gdbinit"
   install::default ".ipython/profile_default/ipython_config.py"
