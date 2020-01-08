midchildan/dotfiles

commit 2db157bab0bd15b5d9084c53149ee6d2022ec7ee

setup: install git post-receive hook for deploying dotfiles remotely

scripts/githooks/post-receive
@@ -1,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This hook is meant to be used in conjuction with git-deploy [1] to deploy
+# dotfiles to a remote with a simple git push.
+#
+# [1] See home/.local/bin/git-deploy
+
+source ./setup.sh

setup.sh
@@ -106,3 +106,7 @@ source "$DOTFILE_DIR/scripts/setup"
   - cmigemo
   - zsh-syntax-highlighting
   - shell: vim +PlugInstall +qall
+
+@install Install Git Hooks
+  - init: true
+  - shell: git config core.hooksPath scripts/githooks

scripts/githooks/.gitignore
@@ -1,0 +1,2 @@
+*
+!post-receive

commit 6c0c9c6bbad85b06fa2dcfee15b55a4c3e17ea73

post-receive: bug fixes and improvements
This replaces 2db157b, which didn't work out for post-receive hooks
because post-receive hooks are always run from $GIT_DIR instead of the
worktree.

CAVEAT:

It is recommended that you run the below command to undo changes caused
by 2db157b.

    $ git config --local --unset-all core.hooksPath

Changes:

- setup: add new @githooks section for registering git hooks to this repo
- setup: add missing "cd $DOTFILE_DIR" to @shell
- post-receive: add more detailed description in comments
- git-deploy: add -b option for specifying branch to push

home/.local/bin/git-deploy
@@ -4,9 +4,12 @@ ENABLE_DRYRUN=
 ENABLE_PREFIX=1
 
 main() {
+  local gitref=HEAD
+
   for n in "$@"; do
     case "$n" in
       -n|--dry-run) ENABLE_DRYRUN=1; shift ;;
+      -b) gitref="$2"; shift; shift ;;
       -r) ENABLE_PREFIX=; shift ;;
       -h|--help) usage ;;
       *) break ;;
@@ -49,7 +52,7 @@ main() {
   run ssh "$remote_host" git init "${remote_path:-.}"
   run ssh "$remote_host" git -C "$remote_path" config receive.denyCurrentBranch updateInstead
   run git remote add "$remote_name" "$remote_host:$remote_path"
-  run git config "remote.$remote_name.push" "+HEAD:master"
+  run git config "remote.$remote_name.push" "+$gitref:master"
 }
 
 usage() {
@@ -59,6 +62,7 @@ usage: $0 [options] [name] [host[:path]]
 options:
   -h, --help		Show usage and exit
   -n, --dry-run		Don't acutally make any changes
+  -b <branchname>	Branch used for pushing (default: current branch)
   -r			Use the given path as-is
 EOF
 

scripts/githooks/post-receive
@@ -5,4 +5,21 @@
 #
 # [1] See home/.local/bin/git-deploy
 
-source ./setup.sh
+# Sample Usage:
+#
+# The following commands would setup a remote repository which can be used to
+# deploy dotfiles in "branch1" to a remote named "host1". After setup is
+# complete, a simple "git push" would apply changes to your dotfiles and
+# trigger the deployment script remotely.
+#
+#     $ git deploy -r -b branch1 host1 user@host1.example.com:.config/dotfiles
+#     $ git push host1
+#     $ ssh -t user@host1.example.com 'cd .config/dotfiles; ./setup.sh --init'
+
+
+# XXX: The following is needed to ensure submodule updates run properly in the 
+#      setup script.
+unset GIT_DIR
+
+# XXX: post-recieve hooks run from $GIT_DIR. See githooks(5) for details.
+source ../setup.sh

scripts/setup
@@ -36,6 +36,7 @@ exec 2>>"$_DOT_LOGFILE"
 @shell() {
   _DOT_SECTION="shell"
   echo "$(tput bold)== ${*:-Run Shell Commands} ==$(tput sgr0)"
+  cd "$DOTFILE_DIR" || dot::_abort "can't enter dir $DOTFILE_DIR"
 }
 
 @install() {
@@ -47,6 +48,12 @@ exec 2>>"$_DOT_LOGFILE"
 @clean() {
   _DOT_SECTION="clean"
   echo "$(tput bold)== ${*:-Clean Dead Symlinks} ==$(tput sgr0)"
+  cd "$DOTFILE_DIR" || dot::_abort "can't enter dir $DOTFILE_DIR"
+}
+
+@githooks() {
+  _DOT_SECTION="githooks"
+  echo "$(tput bold)== ${*:-Register Git Hooks} ==$(tput sgr0)"
   cd "$DOTFILE_DIR" || dot::_abort "can't enter dir $DOTFILE_DIR"
 }
 
@@ -63,6 +70,7 @@ exec 2>>"$_DOT_LOGFILE"
     shell) dot::shell::task "$@" ;;
     install) dot::install::task "$@" ;;
     clean) dot::clean::task "$@" ;;
+    githooks) dot::githooks::task "$@" ;;
   esac
 }
 
@@ -90,12 +98,11 @@ dot::install::task() {
   fi
 
   local dir="$(dirname "$1")"
-  local old_pwd="$(pwd)"
   if [[ -n "$dir" ]] && [[ "$dir" != "." ]]; then
     [[ ! -d ~/"$dir" ]] && mkdir -p ~/"$dir"
-    cd ~/"$dir"
+    pushd ~/"$dir"
   else
-    cd
+    pushd ~
   fi
 
   local fname="$(basename "$1")"
@@ -103,7 +110,7 @@ dot::install::task() {
   dot::_ln "$(dot::_relative_path "$src")" "$fname"
   dot::_print_badge
 
-  cd "$old_pwd"
+  popd
 }
 
 # Run a shell command
@@ -135,6 +142,40 @@ dot::clean::task() {
 
   local filepath=~/"$1"
   [[ ! -L "$filepath" || -e "$filepath" ]] || rm "$filepath"
+  dot::_print_badge
+}
+
+# Symlinks $DOTFILE_DIR/scripts/githooks/$hook into $GIT_DIR/hook
+# Globals:
+#   DOTFILE_DIR
+#   _DOT_LOGFILE
+# Arguments:
+#   hook : hook to install
+# Returns:
+#   None
+dot::githooks::task() {
+  [[ "$#" != 1 ]] && dot::_abort "Wrong number of arguments."
+  [[ "$1" == /* ]] && dot::_abort "Cannot use absoulte path."
+
+  echo -n "Registering $1 hook..."
+  local hook="$1"
+  local src="$DOTFILE_DIR/scripts/githooks/$hook"
+
+  if [[ ! -e "$src" ]]; then
+    echo "$src does not exist" >>"$_DOT_LOGFILE"
+    dot::_print_badge::failed
+    return
+  fi
+  if [[ ! -d .git/hooks ]]; then
+    echo "$DOTFILE_DIR/.git/hooks is not a directory" >> "$_DOT_LOGFILE"
+    dot::_print_badge::failed
+    return
+  fi
+
+  pushd .git/hooks
+  dot::_ln "$(dot::_relative_path "$src")" "$hook"
+  popd
+
   dot::_print_badge
 }
 
@@ -417,6 +458,30 @@ dot::_relative_path() {
   else
     dot::_abort "Needs coreutils, python, ruby, or perl."
   fi
+}
+
+# Wrapper for pushd
+# Returns:
+#   None
+pushd() {
+  builtin pushd "$@" > /dev/null
+}
+
+# Wrapper for popd
+# Returns:
+#   None
+popd() {
+  builtin popd "$@" > /dev/null
+}
+
+# Wrapper for tput
+# Returns:
+#   None
+tput() {
+  case "$TERM" in
+    ""|dumb) return ;;
+    *) command tput "$@" ;;
+  esac
 }
 
 #################

setup.sh
@@ -98,7 +98,8 @@ source "$DOTFILE_DIR/scripts/setup"
   - .xprofile
   - .xmonad
 
-# Will not run unless --init is specified
+# The below will not run unless --init is specified
+
 @packages
   - init: true
   - build-essential
@@ -107,6 +108,6 @@ source "$DOTFILE_DIR/scripts/setup"
   - zsh-syntax-highlighting
   - shell: vim +PlugInstall +qall
 
-@install Install Git Hooks
+@githooks
   - init: true
-  - shell: git config core.hooksPath scripts/githooks
+  - post-receive