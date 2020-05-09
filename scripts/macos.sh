d60ef0adb3e18247c74f98b07b663e20b24a4efd
midchildan <git@midchildan.org>
2020-05-09 16:04:42 +0900

    macos.sh: aesthetic improvements

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -5,35 +5,38 @@
 DOTFILE_DIR="${DOTFILE_DIR:-$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)}"
 source "$DOTFILE_DIR/scripts/setup"
 
-@defaults
-  # battery indicator
-  - com.apple.menuextra.battery ShowPercent -string "YES"
-  - com.apple.menuextra.battery ShowPercent -string "YES"
-  # auto correction
-  - NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
-  - NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-  - NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
-  - NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-  - NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-  # locale
-  - NSGlobalDomain AppleLanguages "en" "ja"
-  - NSGlobalDomain AppleLocale "en_JP"
-  - NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
-  - NSGlobalDomain AppleMetricUnits -bool true
-  # .DS_Store
-  - com.apple.desktopservices DSDontWriteNetworkStores -bool true
-  - com.apple.desktopservices DSDontWriteUSBStores -bool true
-  # dock
-  - com.apple.dock tilesize -int 32
-  - com.apple.dock size-immutable -bool true
-  - com.apple.dock expose-group-apps -bool true
-  # safari
-  - com.apple.Safari AutoOpenSafeDownloads -bool false
-  - com.apple.Safari AutoFillPasswords -bool false
-  - com.apple.Safari AutoFillCreditCardData -bool false
-  - com.apple.Safari IncludeDevelopMenu -bool true
-  - com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-  - com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+@defaults Configure locale
+  - NSGlobalDomain AppleLanguages        array  "en" "ja"
+  - NSGlobalDomain AppleLocale           string "en_JP"
+  - NSGlobalDomain AppleMeasurementUnits string "Centimeters"
+  - NSGlobalDomain AppleMetricUnits      bool   true
+
+@defaults Configure the battery indicator
+  - com.apple.menuextra.battery ShowPercent string "YES"
+
+@defaults Configure the dock
+  - com.apple.dock tilesize          int  32
+  - com.apple.dock size-immutable    bool true
+  - com.apple.dock expose-group-apps bool true
+
+@defaults Configure Safari
+  - com.apple.Safari AutoOpenSafeDownloads  bool false
+  - com.apple.Safari AutoFillPasswords      bool false
+  - com.apple.Safari AutoFillCreditCardData bool false
+  - com.apple.Safari IncludeDevelopMenu     bool true
+  - com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey bool true
+  - com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled bool true
+
+@defaults Tame .DS_Store
+  - com.apple.desktopservices DSDontWriteNetworkStores bool true
+  - com.apple.desktopservices DSDontWriteUSBStores     bool true
+
+@defaults Tame auto-correction
+  - NSGlobalDomain NSAutomaticCapitalizationEnabled     bool false
+  - NSGlobalDomain NSAutomaticDashSubstitutionEnabled   bool false
+  - NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled bool false
+  - NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled  bool false
+  - NSGlobalDomain NSAutomaticSpellingCorrectionEnabled bool false
 
 @install Install keybindings
   # symlinking doesn't work, due to an Apple bug
