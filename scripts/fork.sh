97b2924e6808c9b08ea654d3eb5623e5a692bad9
midchildan <git@midchildan.org>
2020-05-25 21:54:16 +0900

    fork.sh: fix portability issues
    Follow up of ad213dc. fork.sh is one of the very few scripts that
    requires a higher degree of portability without relying on git branches,
    since it's only intended to be run from the master branch. With that
    being said, uses of sed were causing portability issues because of
    BSD/GNU incompabilites which arise when you target both Linux and macOS.
    Therefore, replace sed with perl to fix this problem.

--- scripts/fork.sh
+++ scripts/fork.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Script to change user information
+
+# This is one of the very few scripts where portability matters. Normally I'd
+# deal with portability issues by using different git branches to handle
+# platform differences, freeing the script of the need to handle it by itself.
+# This script however, is intended to be run on the master branch regardless of
+# which platform it's actually being used on. For this reason, this script uses
+# perl instead of sed which is incompatible between GNU and BSD variants.
+
 DOTFILE_DIR="${DOTFILE_DIR:-$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)}"
 
 main() {
@@ -43,13 +52,14 @@ patch::gpg() {
 
   if [[ -z "$GPGKEYID" ]]; then
     # if no keyid is specified, remove keyid setting
-    sed -i -e '/^default-key/d' -e '/./,$!d' "$confpath"
+    perl -i -ne 'print unless /^default-key/' "$confpath"
+    perl -i -pe 'if ($. == 1) { $_ = <> while m/^\n/ }' "$confpath"
   elif grep -q '^default-key' "$confpath"; then
     # if there's an existing keyid setting, replace it
-    sed -i -e "s/^default-key.*/default-key $GPGKEYID/" "$confpath"
+    perl -i -pe "s/^default-key.*/default-key $GPGKEYID/" "$confpath"
   else
     # add keyid setting
-    sed -i -e "1 i default-key $GPGKEYID\n" "$confpath"
+    perl -i -pe "print \"default-key $GPGKEYID\n\n\" if \$. == 1" "$confpath"
   fi
 }
 
