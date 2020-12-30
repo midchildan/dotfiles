7d4a8743a71263453e305fe64d60335a5dcee3ff
midchildan <git@midchildan.org>
2020-12-30 18:26:26 +0900

    feat(setup): usability improvements
    setup.sh:
    
    - run Neovim in headless mode when installing plugins
    
    setup-without-nix.sh:
    
    - add --help
    - add --dry-run
    - run Neovim in headless mode
    - a bit of refactoring

--- scripts/setup-without-nix.sh
+++ scripts/setup-without-nix.sh
@@ -3,7 +3,7 @@
 # Run this script if you ever want to setup the dotfiles without having Nix
 # installed.
 
-set -euo pipefail
+set -Eeuo pipefail
 
 NIX_PROFILE_DIR=~/.nix-profile
 BIN_DIR="$NIX_PROFILE_DIR/bin"
@@ -11,19 +11,38 @@ DATA_DIR="$NIX_PROFILE_DIR/share"
 OPT_DIR="$NIX_PROFILE_DIR/opt"
 SHELLCONF_DIR="$NIX_PROFILE_DIR/etc/profile.d"
 VIMPLUGIN_DIR="$NIX_PROFILE_DIR/share/vim-plugins"
+IS_DRYRUN=
+
+handleError() {
+  trap - ERR
+  msg::err "$(caller): ${BASH_COMMAND}"
+}
+trap handleError ERR
 
 
 main() {
+  for n in "$@"; do
+    case "$n" in
+      -h|--help) usage ;;
+      -n|--dry-run) IS_DRYRUN=1 ;;
+      *) ;;
+    esac
+  done
+
   if has nix-env; then
-    message::warn "Quitting because Nix is installed on this computer"
+    msg::warn "Refusing to run because Nix is installed on this computer."
     exit 1
   fi
   if ! has nvim || [[ ! -f ~/.vim/autoload/plug.vim ]]; then
-    message::err "Quitting because Neovim or vim-plug isn't installed"
+    msg::err "Refusing to run because Neovim or vim-plug isn't installed."
     exit 1
   fi
+  if [[ -n "$IS_DRYRUN" ]]; then
+    msg::warn "This is a dry run. No changes to the filesystem would be made."
+  fi
 
-  message "Installing packages ..."
+  msg "This script will use vim-plug to install packages in $NIX_PROFILE_DIR."
+  msg "Installing packages. This may take a while ..."
 
   mkdir -p \
     "$BIN_DIR" \
@@ -32,9 +51,10 @@ main() {
     "$SHELLCONF_DIR" \
     "$VIMPLUGIN_DIR"
 
-  nvim -u NONE -S <(txt::update-packages.vim)
+  nvim --headless -u NONE -S <(txt::update-packages.vim) 2>&1 \
+    | sed 's/^/nvim: /' >&2
 
-  txt::nix.sh > "$SHELLCONF_DIR/nix.sh"
+  txt::nix.sh | writeFile "$SHELLCONF_DIR/nix.sh"
   txt::fzf-share | writeScript "$BIN_DIR/fzf-share"
   ln -sf "$OPT_DIR/fzf" "$VIMPLUGIN_DIR/fzf"
   ln -sf "$OPT_DIR/coc-nvim" "$VIMPLUGIN_DIR/coc-nvim"
@@ -44,77 +64,92 @@ main() {
     ln -sf "$VIMPLUGIN_DIR/fzf/bin/fzf" "$BIN_DIR/fzf"
   fi
 
-  message "Installation complete"
+  msg "Installation complete"
 
   doctor
 
-  message::tips "Run this script periodically to keep packages up-to-date."
-  message::tips "To uninstall, run 'rm -rf $NIX_PROFILE_DIR'."
-  message::tips "Make sure you uninstall if you ever decide to install Nix later on."
+  msg::tips "Run this script periodically to keep packages up-to-date."
+  msg::tips "To uninstall, run 'rm -rf $NIX_PROFILE_DIR'."
+  msg::tips "Make sure you uninstall if you ever decide to install Nix later on."
 }
 
 doctor() {
-  message "Checking for potential problems ..."
+  msg "Checking for potential problems ..."
 
   local n_diagnosis=0
 
   if has less; then
     if isOlderThan 530 "$(version::less)"; then
-      message::err 'Detected an outdated version of less.'
+      msg::err 'Detected an outdated version of less.'
       (( n_diagnosis += 1 ))
     fi
   else
-    message::err "less not detected."
+    msg::err "less not detected."
     (( n_diagnosis += 1 ))
   fi
 
   if has tmux; then
     if isOlderThan 3.1 "$(version::tmux)"; then
-      message::err 'Detected an outdated version of tmux.'
+      msg::err 'Detected an outdated version of tmux.'
       (( n_diagnosis += 1 ))
     fi
   else
-    message::err "tmux not detected."
+    msg::err "tmux not detected."
     (( n_diagnosis += 1 ))
   fi
 
   if ! has direnv; then
-    message::err "direnv not detected."
+    msg::err "direnv not detected."
     (( n_diagnosis += 1 ))
   fi
 
   if ! has rg; then
-    message::err "ripgrep not detected."
+    msg::err "ripgrep not detected."
     (( n_diagnosis += 1 ))
   fi
 
   if ! has node; then
-    message::err "Node.js not detected. coc.nvim may not run without it."
+    msg::err "Node.js not detected. coc.nvim may not run without it."
     (( n_diagnosis += 1 ))
   fi
 
   if (( n_diagnosis == 0 )); then
-    message::ok "No problems found."
+    msg::ok "No problems found."
   else
-    message::warn "$n_diagnosis problem(s) detected."
-    message::warn "Consider installing the latest versions for missing or outdated programs."
-    message::warn "Alternatively, you could use old configs from git history for outdated programs."
+    msg::warn "$n_diagnosis problem(s) detected."
+    msg::warn "Consider installing the latest versions for missing or outdated programs."
+    msg::warn "For outdated programs, you could also use old configs from git history."
   fi
 }
 
 txt::update-packages.vim() {
 cat <<EOF
 source ~/.vim/autoload/plug.vim
+if "$IS_DRYRUN" != ""
+  fun! plug#begin(dir)
+    echo "Installing packages to " . a:dir
+  endf
+  fun! plug#(repo, ...)
+    echo "Installed " . a:repo
+  endf
+  fun! plug#end()
+  endf
+  command! -nargs=+ -bar Plug call plug#(<args>)
+  command! -bar PlugUpdate call plug#end()
+endif
+
 call plug#begin('$OPT_DIR')
 Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
 Plug 'neoclide/coc.nvim', {'branch': 'release', 'as': 'coc-nvim'}
 Plug 'zsh-users/zsh-syntax-highlighting'
 call plug#end()
+
 PlugUpdate
-only
-redraw
-echo "Waiting for 5 seconds..."
-sleep 5
+if "$IS_DRYRUN" == ""
+  echo
+  %print
+endif
+
 qall!
 EOF
 }
@@ -130,11 +165,6 @@ cat <<EOF
 #!/usr/bin/env bash
 echo "$VIMPLUGIN_DIR/fzf/shell"
 EOF
-}
-
-writeScript() {
-  cat > "$1"
-  chmod +x "$1"
 }
 
 has() {
@@ -159,24 +189,74 @@ isUnicodeTerm() {
   printf "%s" "$LANG" "$(locale LC_CTYPE)" | grep -q UTF-8
 }
 
-message() {
+msg() {
   printf "%s\n" "$*" >&2
 }
 
-message::tips() {
+msg::tips() {
   printf "💡 TIPS: %s\n" "$*" >&2
 }
 
-message::ok() {
+msg::ok() {
   printf "✅ OKAY: %s\n" "$*" >&2
 }
 
-message::err() {
+msg::err() {
   printf "❌ FAIL: %s\n" "$*" >&2
 }
 
-message::warn() {
+msg::warn() {
   printf "🔔 WARN: %s\n" "$*" >&2
+}
+
+writeFile() {
+  if [[ -z "$IS_DRYRUN" ]]; then
+    cat > "$1"
+  else
+    cat > /dev/null # consume stdin
+    msg "Wrote file $1"
+  fi
+}
+
+writeScript() {
+  if [[ -z "$IS_DRYRUN" ]]; then
+    cat > "$1"
+    chmod +x "$1"
+  else
+    cat > /dev/null # consume stdin
+    msg "Wrote script $1"
+  fi
+}
+
+ln() {
+  if [[ -z "$IS_DRYRUN" ]]; then
+    command ln "$@"
+  else
+    msg "Run ln $*"
+  fi
+}
+
+mkdir() {
+  if [[ -z "$IS_DRYRUN" ]]; then
+    command mkdir "$@"
+  else
+    msg "Run mkdir $*"
+  fi
+}
+
+usage() {
+cat <<EOF
+A tool to install minimum required packages without Nix.
+Requires Neovim and vim-plug.
+
+usage: ${0##*/} [options]
+
+options:
+  -h, --help		Print help message and exit
+  -n, --dry-run		Do a trial run without making any persistent changes
+EOF
+
+exit
 }
 
 main "$@"
