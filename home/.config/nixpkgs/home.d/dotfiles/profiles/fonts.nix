22bb1157c1e0e26797779a10867838f7968af0e4
midchildan <git@midchildan.org>
2021-02-04 10:52:59 +0900

    chore(hm): remove upstreamed changes
    NixOS/nixpkgs#108913, NixOS/nixpkgs#108916

--- home/.config/nixpkgs/home.d/dotfiles/profiles/fonts.nix
+++ home/.config/nixpkgs/home.d/dotfiles/profiles/fonts.nix
@@ -2,8 +2,7 @@
 
 with lib;
 
-let extraPkgs = import ../pkgs { inherit pkgs; };
-in {
+{
   options.dotfiles.profiles.fonts.enable = mkOption {
     type = types.bool;
     default = config.dotfiles.profiles.desktop.enable;
@@ -17,11 +16,10 @@ in {
     dotfiles.fonts.enable = mkDefault true;
 
     home.packages = with pkgs; [
-      xkcd-font
-      emacs-all-the-icons-fonts
-    ] ++ (with extraPkgs; [
       fira-code
       powerline-symbols
-    ]);
+      xkcd-font
+      emacs-all-the-icons-fonts
+    ];
   };
 }
