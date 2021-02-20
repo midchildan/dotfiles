fa1fdfec6fcf761a847f2abf7c759a07b663dfa1
midchildan <git@midchildan.org>
2021-02-21 02:20:12 +0900

    feat(hm/fonts): install fonts to ~/Library/Fonts/HomeManager on macOS
    [CAVEATS]
    Fonts previously installed to ~/Library/Fonts would have to be removed
    manually.

--- home/.config/nixpkgs/home.d/dotfiles/modules/fonts.nix
+++ home/.config/nixpkgs/home.d/dotfiles/modules/fonts.nix
@@ -5,60 +5,32 @@ with lib;
 let
   inherit (pkgs.stdenv.hostPlatform) isDarwin;
   isGenericLinux = (config.targets.genericLinux.enable or false);
-  fonts = pkgs.runCommandNoCC "home-manager-fonts" {
+  homeDir = config.home.homeDirectory;
+  fontsEnv = pkgs.buildEnv {
+    name = "home-manager-fonts";
     paths = config.home.packages;
-  } ''
-    mkdir -p $out
-    for path in $paths; do
-      if [[ ! -d "$path/share/fonts" ]]; then
-        continue
-      fi
-      find -L "$path/share/fonts" -type f -print0 | while IFS= read -rd "" f; do
-        ln -s "$f" $out
-      done
-    done
-  '';
-in {
-  options.dotfiles.fonts.enable = mkOption {
-    type = types.bool;
-    default = false;
-    description = ''
-      Whether to enable font support for non-NixOS hosts.
-
-      <warning>
-        <para>
-          On macOS, this may overwrite exsting files in
-          <filename>~/Library/Fonts</filename>. This will be fixed as soon as
-          <link xlink:href="https://github.com/nix-community/home-manager/issues/155">home-manager#155</link>
-          is closed.
-        </para>
-      </warning>
-    '';
+    pathsToLink = "/share/fonts";
   };
+  fonts = "${fontsEnv}/share/fonts";
+in {
+  options.dotfiles.fonts.enable =
+    mkEnableOption "font support for non-NixOS hosts";
 
   config = mkIf config.dotfiles.fonts.enable (mkMerge [
-    (mkIf isGenericLinux {
-      fonts.fontconfig.enable = mkDefault true;
-    })
+    (mkIf isGenericLinux { fonts.fontconfig.enable = mkDefault true; })
     (mkIf isDarwin {
-      warnings = [
-        ("Existing user fonts in ~/Library/Fonts may be overwritten. In most "
-          + "cases, this just means that they would be replaced with the latest"
-          + " versions of the same fonts.")
-      ];
+      home.activation.copyFonts = hm.dag.entryAfter [ "writeBoundary" ] ''
+        copyFonts() {
+          rm -rf ${homeDir}/Library/Fonts/HomeManager
 
-      home.activation = {
-        copyFonts = hm.dag.entryAfter [ "writeBoundary" ] ''
-          copyFonts() {
-            local f
-            find -L "${fonts}" -type f -print0 | while IFS= read -rd "" f; do
-              $DRY_RUN_CMD install $VERBOSE_ARG -Dm644 \
-                -t "$HOME/Library/Fonts" "$f"
-            done
-          }
-          copyFonts
-        '';
-      };
+          local f
+          find -L "${fonts}" -type f -printf '%P\0' | while IFS= read -rd "" f; do
+            $DRY_RUN_CMD install $VERBOSE_ARG -Dm644 -T \
+              "${fonts}/$f" "${homeDir}/Library/Fonts/HomeManager/$f"
+          done
+        }
+        copyFonts
+      '';
     })
   ]);
 }
