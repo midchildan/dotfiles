acd49b2b64f1a5ab8dc4a3735e0f45473d8261a9
midchildan <git@midchildan.org>
2021-12-11 03:30:43 +0900

    fix(nix): apply workaround for socat build issue

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -2,9 +2,14 @@
 
 final: prev:
 
-# FIXME: workaround for https://github.com/NixOS/nixpkgs/pull/146517
 prev.lib.optionalAttrs prev.stdenv.isDarwin {
+  # FIXME: workaround for https://github.com/NixOS/nixpkgs/pull/146517
   clang-tools = prev.clang-tools.override {
     llvmPackages = prev.llvmPackages_12;
   };
+
+  # FIXME: workaround for https://github.com/Homebrew/homebrew-core/pull/88595
+  socat = prev.socat.overrideAttrs (old: {
+    patches = [ ./socat-fix-feature-check-tcpinfo.patch ];
+  });
 }
