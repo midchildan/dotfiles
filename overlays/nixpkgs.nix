9cef03a47cd08a9f1108ca9deef55f72d2b47e1f
midchildan <git@midchildan.org>
2022-01-05 01:11:30 +0900

    chore(nix): remove workaround for fixed issues

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -2,10 +2,4 @@
 
 final: prev:
 
-# FIXME: remove when fix reaches nixpkgs-unstable
-# https://nixpk.gs/pr-tracker.html?pr=147289
-prev.lib.optionalAttrs prev.stdenv.isDarwin {
-  clang-tools = prev.clang-tools.override {
-    llvmPackages = prev.llvmPackages_12;
-  };
-}
+{ }
