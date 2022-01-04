c40acc0f795fa170e0d4fbd837f966745f5a0f3d
midchildan <git@midchildan.org>
2022-01-05 01:14:13 +0900

    fix(nix): add workaround for NixOS/nixpkgs#153486

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -2,4 +2,15 @@
 
 final: prev:
 
-{ }
+rec {
+  # FIXME: remove when fix reaches nixpkgs-unstable
+  # https://nixpk.gs/pr-tracker.html?pr=153486
+  python3 = prev.python3.override {
+    packageOverrides = final: prev: {
+      capstone = prev.capstone.overrideAttrs (_: {
+        sourceRoot = "source/bindings/python";
+      });
+    };
+  };
+  python3Packages = python3.pkgs;
+}
