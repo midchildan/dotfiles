1ce6e880854a7b1933ca846896c2baf08193c871
midchildan <git@midchildan.org>
2022-02-14 04:21:51 +0900

    Revert "feat(overlays): add workaround for NixOS/nixpkgs#158008"
    This reverts commit 3c0213e54cc70ae08f43588f79d6a64505c8e11d.

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -2,21 +2,4 @@
 
 final: prev:
 
-prev.lib.optionalAttrs (prev.stdenv.isDarwin) {
-  # TODO: remove after https://github.com/NixOS/nixpkgs/pull/158008 is merged
-  nghttp3 = prev.nghttp3.overrideAttrs (old: {
-    checkInputs = (old.checkInputs or [ ]) ++ (with prev; [ ncurses ]);
-    meta = old.meta // {
-      platforms = prev.lib.platforms.unix;
-    };
-  });
-
-  # TODO: remove after https://github.com/NixOS/nixpkgs/pull/158008 is merged
-  ngtcp2 = prev.ngtcp2.overrideAttrs (old: {
-    buildInputs = prev.lib.filter (p: p.pname != "jemalloc") old.buildInputs;
-    checkInputs = (old.checkInputs or [ ]) ++ (with prev; [ ncurses ]);
-    meta = old.meta // {
-      platforms = prev.lib.platforms.unix;
-    };
-  });
-}
+{ }
