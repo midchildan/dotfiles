b60ff740e13166a26ab7955b6c67f02eedfdd3c0
midchildan <git@midchildan.org>
2022-03-25 00:54:55 +0900

    chore(nixpkgs): remove obsolete workaround

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -2,31 +2,4 @@
 
 final: prev:
 
-prev.lib.optionalAttrs prev.stdenv.isDarwin rec {
-  python3 = prev.python3.override {
-    packageOverrides = final: prev: {
-      # FIXME: workaround for https://nixpk.gs/pr-tracker.html?pr=159516
-      ipython = prev.ipython.overridePythonAttrs (old: {
-        disabledTests = (old.disabledTests or [ ]) ++ [ "test_clipboard_get" ];
-      });
-
-      # TODO: remove the following workarounds along with the ipython workaround
-      #
-      # These tests seems too flaky on GitHub Actions.
-      passlib = prev.passlib.overridePythonAttrs (old: {
-        disabledTests = (old.disabledTests or [ ]) ++ [ "test_dummy_verify" ];
-      });
-    };
-  };
-  python3Packages = python3.pkgs;
-
-  httpie =
-    let httpieWithGoodDeps = prev.httpie.override { inherit python3Packages; };
-    in
-    httpieWithGoodDeps.overridePythonAttrs (old: {
-      disabledTests = (old.disabledTests or [ ]) ++ [
-        "test_plugins_upgrade"
-        "test_stdin_read_warning"
-      ];
-    });
-}
+prev.lib.optionalAttrs prev.stdenv.isDarwin rec { }
