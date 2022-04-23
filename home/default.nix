7eb4fff8f080f0885b995e3de62e4b37afc70bc8
midchildan <git@midchildan.org>
2022-04-24 01:40:22 +0900

    feat(home): enable declarative management of the Nix user registry

--- home/default.nix
+++ home/default.nix
@@ -1,4 +1,4 @@
-{ inputs }:
+{ inputs }: { lib, ... }:
 
 {
   imports = [
@@ -14,5 +14,20 @@
     ./modules/vim.nix
   ];
 
-  config._module.args.dotfiles = inputs.self;
+  config = {
+    _module.args.dotfiles = inputs.self;
+
+    # NOTE: The following enables declarative management of the Nix user
+    # registry using Home Manager. As a consequence, it makes the Nix registry
+    # read-only, barring the 'nix registry' command from being able to
+    # manipulate the Nix registry. If this is undesirable, disable declarative
+    # management of the Nix registry by placing the following line in your
+    # Home Manager configuration:
+    #
+    #     nix.registry = lib.mkForce { };
+    nix.registry = {
+      nixpkgs.flake = lib.mkDefault inputs.nixpkgs;
+      dotfiles.flake = lib.mkDefault inputs.self;
+    };
+  };
 }
