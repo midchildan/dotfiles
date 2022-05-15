024e81de9b70729ca48bd2885caf731053205a8d
midchildan <git@midchildan.org>
2022-05-15 22:43:09 +0900

    fix(overlays): adjust to latest nixpkgs

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -3,11 +3,20 @@
 final: prev:
 
 prev.lib.optionalAttrs prev.stdenv.isDarwin rec {
-  # FIXME: test seems to be flaky
-  httpie = prev.httpie.overridePythonAttrs (old: {
-    disabledTests = (old.disabledTests or [ ]) ++ [
-      "test_plugins_upgrade"
-      "test_stdin_read_warning"
-    ];
-  });
+  python3 = prev.python3.override {
+    packageOverrides = final: prev: {
+      # FIXME: tests seems to be flaky
+      httpie = prev.httpie.overridePythonAttrs (old: {
+        disabledTestPaths = (old.disabledTestPaths or [ ]) ++ [
+          "tests/test_plugins_cli.py"
+        ];
+
+        disabledTests = (old.disabledTests or [ ]) ++ [
+          "test_stdin_read_warning"
+          "test_daemon_runner"
+        ];
+      });
+    };
+  };
+  python3Packages = python3.pkgs;
 }
