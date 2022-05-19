773a930d938b7a6eb11b23643eb659007e412ec8
midchildan <git@midchildan.org>
2022-05-19 22:57:45 +0900

    chore(nixpkgs): remove httpie fix

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -2,21 +2,4 @@
 
 final: prev:
 
-prev.lib.optionalAttrs prev.stdenv.isDarwin rec {
-  python3 = prev.python3.override {
-    packageOverrides = final: prev: {
-      # FIXME: tests seems to be flaky
-      httpie = prev.httpie.overridePythonAttrs (old: {
-        disabledTestPaths = (old.disabledTestPaths or [ ]) ++ [
-          "tests/test_plugins_cli.py"
-        ];
-
-        disabledTests = (old.disabledTests or [ ]) ++ [
-          "test_stdin_read_warning"
-          "test_daemon_runner"
-        ];
-      });
-    };
-  };
-  python3Packages = python3.pkgs;
-}
+{ }
