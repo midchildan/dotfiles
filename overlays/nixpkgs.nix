a795e834b839bbdfc1f84e2df29811bb4bd43107
midchildan <git@midchildan.org>
2022-07-04 10:56:19 +0900

    chore(overlay): remove obsolete workaround

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -2,25 +2,4 @@
 
 final: prev:
 
-let
-  inherit (prev) lib;
-  inherit (prev.stdenv.hostPlatform) isUnix isStatic;
-in
-lib.optionalAttrs (isUnix && isStatic) {
-  # TODO: Remove once the following issue is fixed:
-  # https://github.com/NixOS/nixpkgs/pull/172892
-  ncurses = prev.ncurses.overrideAttrs (old: {
-    configureFlags = old.configureFlags ++ [
-      # For static binaries, the point is to have a standalone binary with
-      # minimum dependencies. So the following makes sure that binaries using
-      # this package won't depend on a terminfo database located in the Nix
-      # store.
-      "--with-terminfo-dirs=${lib.concatStringsSep ":" [
-        "/etc/terminfo" # Debian, Fedora, Gentoo
-        "/lib/terminfo" # Debian
-        "/usr/share/terminfo" # upstream default, probably all FHS-based distros
-        "/run/current-system/sw/share/terminfo" # NixOS
-      ]}"
-    ];
-  });
-}
+{ }
