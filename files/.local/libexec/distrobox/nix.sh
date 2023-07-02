2a44b717e5b4c93d07358097978eb5ca0cba01ba
midchildan <git@midchildan.org>
2023-07-03 04:18:16 +0900

    improvement(distrobox): preserve SHELL only if it points to the Nix store

--- files/.local/libexec/distrobox/nix.sh
+++ files/.local/libexec/distrobox/nix.sh
@@ -1,23 +1,37 @@
 #!/usr/bin/env sh
 
-dot_shell_name="$(basename "$SHELL")"
+case "$(realpath "$SHELL" 2> /dev/null || :)" in
+  /nix/store/*|/gnu/store/*) ;;
+  *) return ;;
+esac
 
-dot_nix_profiles="
-  $HOME/.nix-profile
-  ${XDG_STATE_HOME:-$HOME/.state}/nix/profile
-  /run/current-system/sw
-"
+case "$1" in
+  preinit)
+    # Backup SHELL because distrobox changes it.
+    dot_shell="$SHELL"
 
-for dot_profile_dir in $dot_nix_profiles; do
-  dot_shell_path="$dot_profile_dir/bin/$dot_shell_name"
+    # Try to prevent distrobox from installing an another shell. Doesn't work
+    # if bash is missing from the container image.
+    SHELL=''
 
-  if [ -x "$dot_shell_path" ]; then
-    SHELL="$dot_shell_path"
-    if ! grep -q "^$dot_shell_path\$" /etc/shells; then
-      echo "$dot_shell_path" | tee -a /etc/shells > /dev/null
+    ;;
+
+  init)
+    if [ -x "$dot_shell" ]; then
+      # Restore SHELL.
+      SHELL="$dot_shell"
+
+      if ! grep -q "^$dot_shell\$" /etc/shells; then
+        echo "$dot_shell" | tee -a /etc/shells > /dev/null
+      fi
+
+      # shellcheck disable=SC2154
+      usermod --shell "$dot_shell" "$container_user_name"
     fi
-    # shellcheck disable=SC2154
-    usermod --shell "$dot_shell_path" "$container_user_name"
-    break
-  fi
-done
+    ;;
+
+  *)
+    printf '[ERROR] nix.sh: unknown script argument: %s' "$1"
+    return 1
+    ;;
+esac
