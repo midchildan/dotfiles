5e88a63253fc5429ca5f73243247c883f40f7494
midchildan <git@midchildan.org>
2023-07-07 03:08:55 +0900

    Revert "fix(darwin): unset non-null defaults in system.defaults"
    This reverts commit 577e905278aff4554f072cf5e3829a585c0d6f15.

--- nix/darwin/modules/config.nix
+++ nix/darwin/modules/config.nix
@@ -45,12 +45,4 @@ in
     (lib.makeBinPath config.environment.profiles)
     "\"$PATH\""
   ];
-
-  # FIXME: somebody's personal preference made it into the upstream defaults
-  system.defaults = {
-    ActivityMonitor = {
-      OpenMainWindow = lib.mkDefault null;
-      ShowCategory = lib.mkDefault null;
-    };
-  };
 }
