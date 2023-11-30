6af4e9a5a65ff3ce7ad0ff160cebe2abcb290b26
midchildan <git@midchildan.org>
2023-11-30 22:47:34 +0900

    fix(flake): add option definition for flakeModules

--- flake.nix
+++ flake.nix
@@ -2,9 +2,12 @@
   description = "Dotfiles from Midchilda";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
--- nix/default.nix
+++ nix/default.nix
@@ -14,6 +14,7 @@ in
 {
   imports = [
     flakeModules.default
+    inputs.flake-parts.flakeModules.flakeModules
     ./apps
     ./darwin
     ./devshells
