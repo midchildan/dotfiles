63203dff3b48d1b5b06b37e7cb1c63e9d62ef1d3
midchildan <git@midchildan.org>
2023-11-30 22:50:25 +0900

    chore: remove nvfetcher

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -5,18 +5,15 @@
     let
       inherit (pkgs.stdenv) isDarwin isLinux;
       nix = pkgs.nixVersions.${config.dotfiles.nix.package};
-      sources = pkgs.callPackage ./_sources/generated.nix { };
     in
     {
       packages = rec {
 
+        cloudfoundry-cli-6 =
+          pkgs.callPackage ./cloudfoundry-cli/default.nix { };
         mmdbctl = pkgs.callPackage ./mmdbctl.nix { };
         neovim = pkgs.callPackage ./neovim.nix { };
         terminfo-collection = pkgs.callPackage ./terminfo-collection.nix { };
-
-        cloudfoundry-cli-6 = pkgs.callPackage ./cloudfoundry-cli/default.nix {
-          inherit sources;
-        };
 
         coc-ansible = pkgs.callPackage ./coc-ansible.nix {
           inherit (pkgs.vimUtils) buildVimPlugin;
