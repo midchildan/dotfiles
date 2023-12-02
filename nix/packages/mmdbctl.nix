a240b50b848195d9b6c801ad11eb059010d55be3
midchildan <git@midchildan.org>
2023-12-03 02:45:42 +0900

    chore(mmdbctl): bump 1.4.2 -> 1.4.4

--- nix/packages/mmdbctl.nix
+++ nix/packages/mmdbctl.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "mmdbctl";
-  version = "1.4.2";
+  version = "1.4.4";
 
   src = fetchFromGitHub {
     owner = "ipinfo";
     repo = "mmdbctl";
     rev = "mmdbctl-${version}";
-    hash = "sha256-idvTjBCHhqsbffUOJHSa7sCR3bfzU8X4ZHog4RxRaec=";
+    hash = "sha256-K+eZ8a/TJrzR4iYu3vbNDlXeRoLuTYEAw0yoRqMJpyI=";
   };
 
-  vendorHash = "sha256-QS5PoIO0SmJK8/yoQTkYBVyvZW8anA4tYqaC1mrVEBU=";
+  vendorHash = "sha256-k+/VPxCDNw8J3opPL7zuU6Kk4rp76UbacvdI/14Iedc=";
 
   doCheck = true;
 
