5ff6502018a7f1d1710855684c6780c95007b6f4
midchildan <git@midchildan.org>
2024-03-01 02:52:27 +0900

    fix(nixpkgs): cutter build failure

--- nix/overlays/nixpkgs.nix
+++ nix/overlays/nixpkgs.nix
@@ -3,6 +3,7 @@
 final: prev:
 
 {
+  # Workaround for https://github.com/NixOS/nixpkgs/pull/286572
   defaultGemConfig = prev.defaultGemConfig // {
     jekyll = attrs:
       let
@@ -17,4 +18,19 @@ final: prev:
 
   # there are two separate derivations for jekyll in Nixpkgs for some reason
   inherit (prev.rubyPackages) jekyll;
+
+  # Fix compile errors
+  cutter = prev.cutter.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ [
+      (prev.fetchpatch {
+        url = "https://github.com/rizinorg/cutter/commit/1a52a7e.patch";
+        hash = "sha256-cn4nv+qQzt6qadVnpy/Dps8OfHm7P3G0HAHMtAccIRU=";
+      })
+      (prev.fetchpatch {
+        url = "https://github.com/rizinorg/cutter/commit/2106551.patch";
+        hash = "sha256-Mgyn7xLwM3JIIRBvbjplGVYiVYCGWTmsW2rYVgYUVrw=";
+        includes = [ "src/widgets/GraphvizLayout.cpp" ];
+      })
+    ];
+  });
 }
