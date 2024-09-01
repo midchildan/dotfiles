dc2dfe1047e9fe4531f7bb6bc4e68a4eb78bf7bd
midchildan <git@midchildan.org>
2024-09-02 02:55:27 +0900

    refactor!(flake): drop support for x86_64-darwin

--- flake.nix
+++ flake.nix
@@ -40,7 +40,6 @@
       systems = [
         "aarch64-darwin"
         "aarch64-linux"
-        "x86_64-darwin"
         "x86_64-linux"
       ];
     };
--- nix/templates/devenv/flake.nix
+++ nix/templates/devenv/flake.nix
@@ -37,7 +37,6 @@
 
       systems = [
         "x86_64-linux"
-        "x86_64-darwin"
         "aarch64-linux"
         "aarch64-darwin"
       ];
