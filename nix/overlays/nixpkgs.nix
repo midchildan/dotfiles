c67a21adfd4c1db02d68296d3b6a1eca676d9932
midchildan <git@midchildan.org>
2024-09-01 22:57:10 +0900

    hotfix(nix-update): backport GitHub fetcher fix

--- nix/overlays/nixpkgs.nix
+++ nix/overlays/nixpkgs.nix
@@ -5,7 +5,18 @@ final: prev:
 let
   pkgsFrom = args: import (prev.fetchFromGitHub args) { inherit (prev) config system; };
 in
-prev.lib.optionalAttrs prev.stdenv.isDarwin {
+{
+  nix-update = prev.nix-update.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ [
+      # github: Add fallback to atom feed if project does not use releases
+      (prev.fetchpatch {
+        url = "https://github.com/Mic92/nix-update/commit/7840def89c26efbb9039efbd4e631b1ade4b2956.patch";
+        hash = "sha256-yUzrwlauSA+2cI0/o4Y09fneGmx7QPB+I5ac9P2L5Xo=";
+      })
+    ];
+  });
+}
+// prev.lib.optionalAttrs prev.stdenv.isDarwin {
   inherit
     (pkgsFrom {
       owner = "NixOS";
