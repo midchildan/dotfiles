dcf915a4af10b18d7c8972cfb97e86f833fdb1b0
midchildan <git@midchildan.org>
2025-03-23 08:03:12 +0900

    fix(fzf-lua): flaky test

--- nix/overlays/nixpkgs.nix
+++ nix/overlays/nixpkgs.nix
@@ -3,4 +3,11 @@
 final: prev:
 
 {
+  luaPackages = prev.luaPackages.overrideScope (
+    _: lprev: {
+      fzf-lua = lprev.fzf-lua.override (old: {
+        patches = (old.patches or [ ]) ++ [ ./fzf-lua-fix-flaky-test.patch ];
+      });
+    }
+  );
 }
