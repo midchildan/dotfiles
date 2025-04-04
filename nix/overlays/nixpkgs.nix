d94d88e0c4c91e8d63635a4e04561c0309eadd76
midchildan <git@midchildan.org>
2025-04-04 10:27:40 +0900

    revert: fzf-lua workaround

--- nix/overlays/nixpkgs.nix
+++ nix/overlays/nixpkgs.nix
@@ -3,9 +3,4 @@
 final: prev:
 
 {
-  vimPlugins = prev.vimPlugins // {
-    fzf-lua = prev.vimPlugins.fzf-lua.overrideAttrs (old: {
-      patches = (old.patches or [ ]) ++ [ ./fzf-lua-fix-flaky-test.patch ];
-    });
-  };
 }
